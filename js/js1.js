2dad773eb01f50436d193c719f68b3cf31cb0a6f
= <=>
2023-10-05 17:29:50 -0500

    fin seccion carrito

--- js/js1.js
+++ js/js1.js
@@ -5,12 +5,14 @@ const vaciarCarrito = document.querySelector("#vaciar-carrito");
 const listaCursos = document.querySelector(`#lista-cursos`); //selector de lista cursos
 let articulosCarrito = []; // este sera el arreglo que tendra nuestros articulos en el carrito lo llenamos en la funcion leerDatosCursos
 
+carritoHtml();
 //funcion para registrar todos los eventListener
 registrarEventListeners();
 
 function registrarEventListeners() {
   //Cuando se agrega un curso precionando el boton agregar un curso
   listaCursos.addEventListener(`click`, agregarCurso);
+  carrito.addEventListener('click', eliminarElemento)
 }
 
 //funciones
@@ -34,7 +36,6 @@ function agregarCurso(event) {
 // por el momento estamos accediendo al boton pero devemos acceder a los demas elementos del curso
 // la siguiente funcion lee el contrenido del html al que le dimos click y trae la informacion del curso
 function leerDatosCurso(curso) {
-  console.log(curso);
   //objeto
   infoCurso = {
     imagen: curso.querySelector(`#img-curso`).src,
@@ -62,10 +63,22 @@ function leerDatosCurso(curso) {
     articulosCarrito = [...articulosCarrito, infoCurso];
   }
 
-  console.log(articulosCarrito);
   carritoHtml();
 }
 
+//eliminar elemento del carrito o eliminar carrito 
+function eliminarElemento (event){
+  if(event.target.classList.contains("btn__eliminar")){
+    idCurso = event.target.getAttribute("data-id");
+    
+    articulosCarrito = articulosCarrito.filter((articulo) => articulo.id != idCurso);
+    carritoHtml();
+  }
+  else if(event.target.classList.contains("btn__variar__carrito")){
+    articulosCarrito = [];
+    limpiarHTML()
+  }
+}
 /* ahora esta funcion es para mostrar el carrito en html*/
 function carritoHtml() {
   /* limpiar el html antes de agreagr el contenido del carrito*/
@@ -92,10 +105,9 @@ function carritoHtml() {
                  ${cantidad}
             </td>
             <td class = "accioncarrito">
-                <button class = "btn__eliminar"> Eliminar </button>
+                <button class="btn__eliminar" data-id = ${curso.id}> Eliminar </button>
             </td>
             </tr>
-            
          `;
     //AGREGAR EL HTML DEL CARRITO EN EL TBODY
     contenedorCarrito.appendChild(row);
